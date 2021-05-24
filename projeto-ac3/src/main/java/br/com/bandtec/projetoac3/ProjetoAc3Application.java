4f3d4b7e3125a321f60b925cabf672e7874f1c2b
Douglas Dourado <61556868+DouglasNova@users.noreply.github.com>
2021-05-23 22:09:40 -0300

    Inico

---
+++ projeto-ac3/src/main/java/br/com/bandtec/projetoac3/ProjetoAc3Application.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.projetoac3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAc3Application.class, args);
+	}
+
+}
